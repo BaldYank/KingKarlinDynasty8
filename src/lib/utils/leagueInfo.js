8b1995b1a41ed1a1166e4784e2c1657365b0bb4b
BaldYank <143309723+BaldYank@users.noreply.github.com>
2023-08-27 00:52:22 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -30,7 +30,7 @@ export const managers = [
       "name": "Cody Karlin",
       "location": "Portland, Oregon", // (optional)
       "bio": "Lorem ipsum...",
-      "photo": "Cody_ducksfin.png", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/Cody_ducksfin.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2017, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "lac", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
