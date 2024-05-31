bc32bfd7656cd6632f6257fac1a004797194287c
BaldYank <143309723+BaldYank@users.noreply.github.com>
2024-05-30 22:40:35 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
-export const leagueID = "969452659784687616"; // your league ID
-export const leagueName = "The Jeff Sucks League"; // your league name
+export const leagueID = "1048456241095352320"; // your league ID
+export const leagueName = "Baby C Diaper Fund"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
